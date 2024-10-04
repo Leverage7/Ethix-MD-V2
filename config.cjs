4e6aed19c64c15d34962e4c279f663c23b91dd38
Leverage7 <lskssjrage@gmail.com>
2024-10-04 12:32:49 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&qiujgzAe",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&9pYpTNT4",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
